1cead2aa3e78bf02d6bf64f58926d1426926b9f2
Schachtschneider <kschach2@10-109-93-116.comd.uic.edu>
2018-07-13 10:20:44 -0500

    finalized Cirrhosis miRNA scripts

--- Cirrhosis/miRNA/mirdeep2/mirdeep2.sh
+++ Cirrhosis/miRNA/mirdeep2/mirdeep2.sh
@@ -19,3 +19,26 @@ module load Bowtie/1.2.0-IGB-gcc-4.9.4
 
 /home/a-m/kschach2/bin/mirdeep2/bin/miRDeep2.pl /home/a-m/kschach2/Cirrhosis/miRNAseq/mirdeep2_output/combined_collapsed.fa /home/a-m/kschach2/porcine_assembly/mirdeep2.Sscrofa11.1.dna.toplevel.fa /home/a-m/kschach2/Cirrhosis/miRNAseq/mirdeep2_output/combined_collapsed.arf Sus_scrofa_miRNA.fa Homo_sapiens_miRNA.fa none 2>report.log
 
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0112_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0113_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0921_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0925_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0993_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0994_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0995_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r/home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0996_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0997_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+quantifier.pl -p /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/pres.fa -m /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/mature.fa -r /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2_output/0998_collapsed.fa -s /home/labs/schook_lab/ayasmi2/Cirrhosis/miRNAseq/mirdeep2/mirna_results_16_05_2018_t_15_43_57/star.fa
+
+#combine expression (read counts) into single matrix, run normalization/DE analysis in R
+R CMD BATCH miRNA.R
+
